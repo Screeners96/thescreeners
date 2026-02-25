3c88ce4b2c8b122cebb07e4bad1dcbc82addabde
caringculture <55089976+3177505@users.noreply.github.com>
2026-02-25 11:29:06 +0100

    update node deploy

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,18 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ['sanity', '@sanity/vision', 'next-sanity'],
+  transpilePackages: ['sanity', '@sanity/vision', 'next-sanity', '@sanity/icons'],
   images: {
     domains: ['cdn.sanity.io'],
+  },
+  webpack: (config, { isServer }) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@sanity/icons': path.resolve(__dirname, 'node_modules/@sanity/icons'),
+    }
+    return config
   },
 }
 
